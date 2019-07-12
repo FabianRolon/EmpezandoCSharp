63d2dc61c0e1d1d70a4fbb2d0c8571ce16cf9e9a
FabianRolon <fabi18fr@gmail.com>
2019-07-11 23:11:10 -0300

    primer juego con c#, snake, terminado

---
+++ snakeGame/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace snakeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.canva = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.puntos = new System.Windows.Forms.Label();
+            this.bucle = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.canva)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // canva
+            // 
+            this.canva.Location = new System.Drawing.Point(15, 16);
+            this.canva.Name = "canva";
+            this.canva.Size = new System.Drawing.Size(780, 390);
+            this.canva.TabIndex = 0;
+            this.canva.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(121, 423);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 33);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Puntaje";
+            // 
+            // puntos
+            // 
+            this.puntos.AutoSize = true;
+            this.puntos.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.puntos.Location = new System.Drawing.Point(240, 423);
+            this.puntos.Name = "puntos";
+            this.puntos.Size = new System.Drawing.Size(31, 33);
+            this.puntos.TabIndex = 2;
+            this.puntos.Text = "0";
+            // 
+            // bucle
+            // 
+            this.bucle.Enabled = true;
+            this.bucle.Tick += new System.EventHandler(this.Bucle_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 480);
+            this.Controls.Add(this.puntos);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.canva);
+            this.Name = "Form1";
+            this.Text = "Snake";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.canva)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox canva;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label puntos;
+        private System.Windows.Forms.Timer bucle;
+    }
+}
+
---
+++ snakeGame/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace snakeGame
+{
+    public partial class Form1 : Form
+    {
+        Cola cabeza;
+        Comida comida;
+        Graphics g;
+        int puntaje = 0;
+        int cuadro = 10;
+        int xdir = 0;
+        int ydir = 0;
+        Boolean ejex = true;
+        Boolean ejey = true;
+        public Form1()
+        {
+            InitializeComponent();
+            cabeza = new Cola(10, 10);
+            comida = new Comida();
+            g = canva.CreateGraphics();
+        }
+        public void movimiento()
+        {
+            cabeza.setxy(cabeza.verX() + xdir, cabeza.verY() + ydir);
+        }
+        private void Bucle_Tick(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            cabeza.dibujar(g);
+            comida.dibujar(g);
+            movimiento();
+            choqueCuerpo();
+            choquePared();
+            if (cabeza.interseccion(comida))
+            {
+                comida.colocar();
+                cabeza.meter();
+                puntaje++;
+                puntos.Text = puntaje.ToString();
+            }
+        }
+        public void finDeJuego()
+        {
+            puntaje = 0;
+            puntos.Text = "0";
+            ejex = true;
+            ejey = true;
+            xdir = 0;
+            ydir = 0;
+            cabeza = new Cola(10, 10);
+            comida = new Comida();
+            MessageBox.Show("Perdiste");
+        }
+        public void choquePared()
+        {
+            if (cabeza.verX() < 0 || cabeza.verX() > 770 || cabeza.verY() < 0 || cabeza.verY() > 380)
+            {
+                finDeJuego();
+            }
+        }
+        public void choqueCuerpo()
+        {
+            Cola temp;
+            try
+            {
+                temp = cabeza.verSiguiente().verSiguiente();  
+            }catch(Exception err)
+            {
+                temp = null;
+            }
+            while(temp != null)
+            {
+                if (cabeza.interseccion(temp))
+                {
+                    finDeJuego();
+                }
+                else
+                {
+                    temp = temp.verSiguiente();
+                }
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(ejex)
+            {
+                if(e.KeyCode == Keys.Up)
+                {
+                    ydir = -cuadro;
+                    xdir = 0;
+                    ejex = false;
+                    ejey = true;
+                }
+                if (e.KeyCode == Keys.Down)
+                {
+                    ydir = cuadro;
+                    xdir = 0;
+                    ejex = false;
+                    ejey = true;
+                }
+
+            }
+            if (ejey)
+            {
+                if(e.KeyCode == Keys.Right)
+                {
+                    ydir = 0;
+                    xdir = cuadro;
+                    ejex = true;
+                    ejey = false;
+                }
+                if (e.KeyCode == Keys.Left)
+                {
+                    ydir = 0;
+                    xdir = -cuadro;
+                    ejex = true;
+                    ejey = false;
+                }
+            }
+        }
+    }
+}
